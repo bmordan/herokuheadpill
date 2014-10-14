7c29d9e8cebf9592c18566af6860b1e956cc9bb9
Bernard Mordan <bernardmordan@gmail.com>
2014-10-14 17:41:51 +0100

    Connections to databases added to DataMapper for Sinatra app

--- app/main.rb
+++ app/main.rb
@@ -4,6 +4,8 @@ require 'sinatra'
 require 'data_mapper'
 require 'rack-protection'
 
+ENV["RACK_ENV"] == "production" ? DataMapper.setup(:default, ENV["DATABASE_URL"]) : DataMapper.setup(:default, "postgres://localhost/headpill_#{env}")
+
 get '/' do
   "Hello World"
 end
